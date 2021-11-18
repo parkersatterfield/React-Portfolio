951e252cb74b9a3949d522770ef5705462eac213
PSatt <parkers205@gmail.com>
2021-11-18 10:27:25 -0600

    base files

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import Nav from '../Nav/Nav'
+
+function Header() {
+    return (
+        <div>
+            <h1>Parker Satterfield</h1>
+            <img type='svg' src="../../assets/images/bg.svg" alt="bg" />
+            <Nav />
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,14 @@
+function Nav() {
+    return (
+        <div className = 'nav'>
+            <ul>
+                <li><a href="#aboutme">About Me</a></li>
+                <li><a href="#mywork">My Work</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="../assets/images/resume.pdf">Resume</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav;
