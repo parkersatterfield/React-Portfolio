15bac6dc55e2b79d7d2d753795f5e3030941a1eb
PSatt <parkers205@gmail.com>
2021-12-17 14:38:56 -0600

    reworked mywork page

--- src/components/MyWork/MyWork.js
+++ src/components/MyWork/MyWork.js
@@ -29,31 +29,67 @@ const dibsLink = "https://dibsfunding.com/"
 function MyWork() {   
     const transition = {duration: 1, ease: [0.43, 0.13, 0.23, 0.96]}
 
+    // document.body.style = `
+    // background-image: url(${blob}); 
+    // background-repeat: no-repeat;
+    // background-position: top 90% left -300px;
+    // background-size: 160vh;
+    // `;
+
     document.body.style = `
-    background-image: url(${blob}); 
-    background-repeat: no-repeat;
-    background-position: top 90% left -300px;
-    background-size: 160vh;
-    `;
+    background-image: ""; `;
     
     // document.body.style = 'background: #ACA9BB;';
     return (
-        <motion.div 
-            className = "portfolioSection"
-            exit={{opacity: 0}}
-            initial={{opacity: 0}}
-            animate={{ opacity: 1 }}
-            transition={transition}>
-            <h1>My Work</h1>
-            <div className='body'>
-                <div className = 'projectContainer'>
+        // <motion.div 
+        //     className = "portfolioSection"
+        //     exit={{opacity: 0}}
+        //     initial={{opacity: 0}}
+        //     animate={{ opacity: 1 }}
+        //     transition={transition}>
+        //     <div className='body'>
+                // <div className = 'projectContainer'>
+                //     <Project projectLink = {reloc8Link} projectImage={reloc8Image} projectName = {reloc8Name}/>
+                //     <Project projectLink = {dibsLink} projectImage={dibsImage} projectName = {dibsName}/>
+                //     <Project projectLink = {weatherLink} projectImage={weatherImage} projectName = {weatherName}/>
+                //     <Project projectLink = {cmsLink} projectImage={cmsImage} projectName = {cmsName}/>
+                // </div>
+        //     </div>
+        // </motion.div>
+
+        <div className="mainContainer">
+            <div className = 'projectContainer'>
+                <div className="flexRow">
                     <Project projectLink = {reloc8Link} projectImage={reloc8Image} projectName = {reloc8Name}/>
+                    <div className="desc">
+                        <h4>Reloc8</h4>
+                        <p>Reloc8 was the first project that we did for bootcamp. As a team, we wanted to put together 2 APIs and throw them together in an app. Originally, we wanted to use the Zillow API and the Monster API to allow for users to search a job and then receive housing results based on the job location, but we were having some issued with both the Monster and Zillow public API keys. We ended up going with the Yelp API and hard coding a list of jobs</p>
+                    </div>
+                </div>
+                <div className="flexRow switch">
                     <Project projectLink = {dibsLink} projectImage={dibsImage} projectName = {dibsName}/>
+                    <div className="desc">
+                        <h4>Dibs</h4>
+                        <p>While working at my first job out of school, I decided I wanted to pursue a startup project that my friend and I had been working on for a little while. Without knowing anything about web design, I decided to try and learn how to code and build an MVP using bubble.io. Dibs is an online social networking and seed funding platform for early stage startups. </p>
+                    </div>
+                </div>
+                <div className="flexRow">
                     <Project projectLink = {weatherLink} projectImage={weatherImage} projectName = {weatherName}/>
+                    <div className="desc">
+                        <h4>Wether Dashboard</h4>
+                        <p>Building a weather app is a right of passage for front end developers. This was one of the first bootcamp assignments. We were given an image of what it was supposed to look like and the API it was supposed to use and told to build it.</p>
+                    </div>
+                </div>
+                <div className="flexRow switch">
                     <Project projectLink = {cmsLink} projectImage={cmsImage} projectName = {cmsName}/>
+                    <div className="desc">
+                        <h4>Employee Tracker CMS</h4>
+                        <p>Here is another bootcamp assignment. This was my first Node.js terminal app. It utilizes MySQL and Node. In the future, I'd like to build a deployed front end for this as a little SaaS project. </p>
+                    </div>
                 </div>
             </div>
-        </motion.div>
+
+        </div>
     )
 }
 
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -6,7 +6,11 @@ function Project(props) {
         <div className="project">
             <a href={props.projectLink} target="_blank" rel="noreferrer">
                 <img id="example" src={props.projectImage} alt={props.projectName} />
-                <figcaption>{props.projectName}</figcaption>
+                {/* <div className="overlay">
+                    <h1 className="name">{props.projectName}</h1>
+                    <h2>{props.projectTech}</h2>
+                </div> */}
+                {/* <figcaption>{props.projectName}</figcaption> */}
             </a>
         </div>
     )
